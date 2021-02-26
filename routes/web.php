d443ae31aab64c370b0aabfcf19f176fa46c7513
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-02-26 21:27:47 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ListController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::match(['get', 'post'],'/', [ListController::class, 'index']);
